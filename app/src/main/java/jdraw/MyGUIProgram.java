6e608d04b95616c3b7c30d801db4f54cb10de55d
PAN-Chuwen <panchuwen2000@gmail.com>
2022-11-29 14:28:42 +0800

    Add AWT cases

---
+++ app/src/main/java/jdraw/MyGUIProgram.java
@@ -0,0 +1,33 @@
+package jdraw;
+
+import java.awt.*;  // Using Frame class in package java.awt
+
+// A GUI program is written as a subclass of Frame - the top-level container
+// This subclass inherits all properties from Frame, e.g., title, icon, buttons, content-pane
+public class MyGUIProgram extends Frame {
+
+   // private variables
+    Button btn;
+    Label lbl;
+
+   // Constructor to setup the GUI components and event handlers
+    public MyGUIProgram() { 
+        this.setLayout(new FlowLayout());
+        lbl = new Label("hi!");
+
+
+
+
+        this.add(lbl);
+        this.setVisible(true);
+
+        this.pack();
+
+    }
+
+   // The entry main() method
+    public static void main(String[] args) {
+      // Invoke the constructor (to setup the GUI) by allocating an instance
+        new MyGUIProgram();
+    }
+}
